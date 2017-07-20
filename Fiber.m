23ffa92268b77a7ce1010bec41254ee22e2b250c
Jordan Graves <jordanleegraves@icloud.com>
2017-07-20 12:59:46 -0500

    mostly working, TODO: unmerge fibers

--- Fiber.m
+++ Fiber.m
@@ -4,17 +4,25 @@ classdef Fiber<handle
     
     properties
         points = [];
+        was_extended = 0;
     end
     
     methods
         function addPoint(obj, point) 
             obj.points = [obj.points; point];
+            obj.was_extended = 1;
         end
         
+        %% Returns a point calculated from a trajectory if this fiber has more than 
+        % one points. Returns a single point otherwise
         function virtual_point = getNextVirtualPoint(obj)
-            p1 = [obj.points(end-1,:)];
-            p2 = [obj.points(end,:)];
-            virtual_point = 2*p2 - p1;
+            if length(obj.points(:,1)) > 1
+                p1 = [obj.points(end-1,:)];
+                p2 = [obj.points(end,:)];
+                virtual_point = 2*p2 - p1;
+            else
+                virtual_point = [obj.points(1,:)];
+            end
         end
     end 
     
--- collectCentroids.m
+++ collectCentroids.m
@@ -1,18 +1,26 @@
-function [ slices ] = collectCentroids( folder_path)
-    slices = {};
+function [ slices ] = collectCentroids( folder_path) 
 %% Import images sequentially and Converts Centroids into Data Points
-    for i=1:15
+% slices: a cell array of arrays of x,y points
+%    slices(1) is an array of all x,y points slice 1...etc
+slices = {};
+    for i=1:96
         if i<10
-            filename=sprintf(strcat(folder_path,'Slide0%d.tiff'),i);
+            filename=sprintf(strcat(folder_path,'FOV 1 XZ PLANE000%d.tif'),i);
         else
-            filename=sprintf(strcat(folder_path,'Slide%d.tiff'),i); 
+            filename=sprintf(strcat(folder_path,'FOV 1 XZ PLANE00%d.tif'),i); 
         end
     
         I=imread(filename);
-        I=imcomplement(I); %centroid only works for white shapes
+        %I=imcomplement(I); %centroid only works for white shapes
         I=im2double(I);
-        I=I(:,:,1);
+        I=rgb2gray(I);
+        I = medfilt2(I);
+        
+        if i==47
+           disp('ok') 
+        end
 
+        I=I(:,:,1);
         %labels each invdividual white dot for tracking
         labelarray=bwlabel(I);
 
@@ -21,7 +29,8 @@ function [ slices ] = collectCentroids( folder_path)
 
         p = [];
         for j=1:length(s)
-            point = s(j).Centroid; 
+            point = s(j).Centroid;
+            point = [point i];
             p = [p; point];
         end
         slices{i} = p;
